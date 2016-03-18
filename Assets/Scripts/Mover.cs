a8abb24ca5164e2ef4e4da6b04837bff6600b46a
Gilles Hoarau <unicolored@gmail.com>
2016-03-18 23:54:00 +0100

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = transform.forward * speed;
+
+        
+
+    }
+
+    public void Faster()
+    {
+        speed = speed * 1; 
+    }
+
+    public void Reset()
+    {
+        //speed = initialSpeed;
+    }
+}
