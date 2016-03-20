91943de72295a50d71c5bd6e0efed224d9382e5e
Gilles Hoarau <unicolored@gmail.com>
2016-03-21 00:23:23 +0100

    Scrolling Background

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -26,7 +26,7 @@ public class GameController : MonoBehaviour
         restartText.text = "";
         gameOverText.text = "";
         score = 0;
-        hazard.GetComponent<Mover>().Reset();
+        //hazard.GetComponent<Mover>().Reset();
         UpdateScore ();
         StartCoroutine (SpawnWaves ());
     }
@@ -49,13 +49,14 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards [Random.Range(0,hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
-            hazard.GetComponent<Mover>().Faster();
+            //hazard.GetComponent<Mover>().Faster();
             if (gameOver)
             {
                 restartText.text = "Press 'R' for Restart";
